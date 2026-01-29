9125c717c91083a8af7a43f56bf74b862f15a4ac
TheDev-M <nagy.marton.2002@gmail.com>
2026-01-29 11:18:10 +0100

    Initial commit

---
+++ src/main/java/com/codecool/getalife/GetALifeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.getalife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetALifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetALifeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/getalife/GetALifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.getalife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetALifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
