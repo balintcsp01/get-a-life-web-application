448214d8b49a6c140e103acbfa58ae5ce8416ee6
TheDev-M <nagy.marton.2002@gmail.com>
2026-02-11 11:57:18 +0100

    Refactor `Category` handling: switch `getAll` response from `List` to `Set`, remove unused repository methods, and simplify DTO structure.

--- src/main/java/com/codecool/getalife/model/dto/category/CategoryNameResponse.java
+++ src/main/java/com/codecool/getalife/model/dto/category/CategoryNameResponse.java
@@ -1,8 +1,3 @@
 package com.codecool.getalife.model.dto.category;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record CategoryNameResponse(String name) {
-
-}
+public record CategoryNameResponse(String name) {}
--- src/main/java/com/codecool/getalife/repository/CategoryRepository.java
+++ src/main/java/com/codecool/getalife/repository/CategoryRepository.java
@@ -1,15 +1,9 @@
 package com.codecool.getalife.repository;
 
 import com.codecool.getalife.model.Category;
-import com.codecool.getalife.model.dto.category.CategoryNameResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-
-    Optional<Category> findCategoryByName(String name);
-    Optional<CategoryNameResponse> findCategoryById(Long id);
 
     boolean existsCategoryByNameIgnoreCase(String name);
 }
